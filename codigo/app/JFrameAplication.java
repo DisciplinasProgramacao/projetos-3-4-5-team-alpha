a5e05715b71718034069a9d3a69a474e2d0479cd
Lucas Lutti <lucaspicinin0@gmail.com>
2022-10-14 17:53:47 -0300

    Update Criando aplicação Jframe

---
+++ codigo/app/JFrameAplication.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+    public class JFrameAplication extends JFrame {
+        public static void main(String[] args) {
+    
+            JFrameAplication window = new JFrameAplication();
+            window.setSize(500,500);
+            window.setVisible(true);
+            //Objetos swing 
+        
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            
+        }
+    }
+
